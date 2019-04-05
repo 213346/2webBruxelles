9b7306b4f223d49fa8ba349f7d83cc1e5f8ab89d
282347 <282347@supinfo.com>
2019-04-05 13:15:49 +0200

    implementation de toutes les fonctionnalités

--- TP6/include/login.php
+++ TP6/include/login.php
@@ -1,7 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: leonie
- * Date: 05-04-19
- * Time: 10:45
- */
+    $user = null;
+    if ( isset($_COOKIE["user"]) && $_COOKIE["user"] !=null){
+        $user = json_decode( $_COOKIE["user"]);
+    }
+?>
+
+<div class="row d-flex justify-content-center">
+    <form class="col-12 col-sm-6 p-5" method="post" action="../server/login.php">
+        <?php if(isset($_GET["error"])
+            && isset($_GET["tab"])
+            && $_GET["tab"]=="register"
+        ){ ?>
+            <div class="alert alert-danger">
+                Bad credential !
+            </div>
+        <?php } ?>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input autocomplete="off" class="form-control"
+                   value="<?php if($user!=null) echo $user["email"]; ?>"
+                   type="email" id="email" name="email"
+                   placeholder="exemple@supinfo.com"/>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input autocomplete="off" class="form-control"
+                   value="<?php if($user!=null) echo $user["email"]; ?>"
+                   type="password" id="password" name="password"
+                   placeholder="com554dhs-cbv"/>
+        </div>
+        <div class="form-group">
+            <input autocomplete="off"
+                   type="checkbox" id="remember" name="remember"/>
+            <label for="remember">Remember me</label>
+
+        </div>
+        <div class="form-group">
+            <input class="btn btn-primary"
+                   type="submit" id="btnLogin" name="btnLogin"
+                   value="Login"/>
+        </div>
+    </form>
+</div>
--- TP6/include/register.php
+++ TP6/include/register.php
@@ -1,7 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: leonie
- * Date: 05-04-19
- * Time: 10:46
- */
+<div class="row d-flex justify-content-center">
+    <form method="post" class="col-12 col-sm-6 p-5" action="../server/register.php">
+        <?php if(isset($_GET["error"])
+            && isset($_GET["tab"])
+            && $_GET["tab"]=="register"
+        ){ ?>
+             <div class="alert alert-danger">
+                 Error !
+             </div>
+        <?php } ?>
+        <div class="form-group">
+            <label for="username">username</label>
+            <input autocomplete="off" class="form-control"
+                   type="text" id="username" name="username"
+                   placeholder="toto"/>
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input autocomplete="off" class="form-control"
+                   type="email" id="email" name="email"
+                   placeholder="exemple@supinfo.com"/>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input autocomplete="off" class="form-control"
+                   type="password" id="password" name="password"
+                   placeholder="com554dhs-cbv"/>
+        </div>
+        <div class="form-group">
+            <input class="btn btn-primary"
+                   type="submit" id="btnRegister" name="btnRegister"
+                   value="Register"/>
+        </div>
+    </form>
+</div>
